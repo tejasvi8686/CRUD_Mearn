28e62aeae6ec2f5183ecabed39d29940d9351761
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-22 16:15:53 +0530

    bug fix in  deployment

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js"
+import path from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json()); // allows us to expcet JSON data in the req.body
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use("/api/products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
   connectDB();
